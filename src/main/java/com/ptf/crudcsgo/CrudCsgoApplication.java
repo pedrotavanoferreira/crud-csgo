fc9ef133f5f0e3353fbee449e4f7c0a4b725c1c8
Pedro Tavano Ferreira <pedro_ferry@outlook.com>
2022-12-28 15:53:03 -0300

    adiciona projeto

---
+++ src/main/java/com/ptf/crudcsgo/CrudCsgoApplication.java
@@ -0,0 +1,13 @@
+package com.ptf.crudcsgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCsgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCsgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ptf/crudcsgo/CrudCsgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptf.crudcsgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCsgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
