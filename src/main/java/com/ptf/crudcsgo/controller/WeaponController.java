112b17ec7862933f46407ca6acbb89ba28e992c2
Pedro Tavano Ferreira <pedro_ferry@outlook.com>
2023-01-16 09:58:00 -0300

    adiciona service

--- src/main/java/com/ptf/crudcsgo/controller/WeaponController.java
+++ src/main/java/com/ptf/crudcsgo/controller/WeaponController.java
@@ -1,5 +1,6 @@
 package com.ptf.crudcsgo.controller;
 
+import com.ptf.crudcsgo.service.WeaponService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/weapon")
 public class WeaponController {
 
+    private WeaponService weaponService;
+
+    public WeaponController(WeaponService weaponService) {
+        this.weaponService = weaponService;
+    }
+
     @GetMapping
     public String getWeapon(){
-        return "vou retornar dragonlore";
+        return this.weaponService.getWeapon();
     }
 
 }
---
+++ src/main/java/com/ptf/crudcsgo/service/WeaponService.java
@@ -0,0 +1,10 @@
+package com.ptf.crudcsgo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeaponService {
+    public String getWeapon(){
+        return "dragonlore";
+    }
+}
