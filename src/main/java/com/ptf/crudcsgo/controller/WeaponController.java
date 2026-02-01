3bad146837a2301b0e45c74a59f17a7d5ce83d45
Pedro T. Ferreira <pedro_ferry@outlook.com>
2026-02-01 20:58:10 -0300

    adiciona projeto

--- src/main/java/com/ptf/crudcsgo/controller/WeaponController.java
+++ src/main/java/com/ptf/crudcsgo/controller/WeaponController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/weapons")
 public class WeaponController {
